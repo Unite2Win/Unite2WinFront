817e906ee155432b874b949e2e0f31aafb28ccfb
AimarMartin <aimarmartin47@gmail.com>
2023-05-26 00:28:14 +0200

    Validaciones Inicio y añadida barra de progreso y datos del usuario

--- main/src/app/usuario/inicio/inicio.component.ts
+++ main/src/app/usuario/inicio/inicio.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Objetivo } from '../interfaces/objetivo';
 import { ObjetivosService } from '../services/objetivos.service';
+import { globales } from 'common/globales';
+import { Usuario } from '../interfaces/usuarioModel';
 
 @Component({
   selector: 'app-inicio',
@@ -10,11 +12,19 @@ import { ObjetivosService } from '../services/objetivos.service';
 export class InicioComponent implements OnInit {
   objetivo: Objetivo;
   objetivos: Objetivo[] = [];
-
+  ObjetivosTitulo:String = 'Objetivos Activos:';
+  TextoBoton:String = 'Ver todos los objetivos';
+  globales: globales;
+  usuario:Usuario = globales.usuarioLogueado;
   constructor(private objetivosService: ObjetivosService) { }
   async ngOnInit() {
-
     await this.objetivosService.getObjetivos();
-  this.objetivos = this.objetivosService.objetivosBd;
+  this.objetivos = this.objetivosService.objetivosBd;  
+  console.log(this.objetivos.length);
+  if (this.objetivos.length == 0) {
+    this.ObjetivosTitulo = 'Aun no hay objetivos';
+    this.TextoBoton = 'Crear objetivos';
+  };
 }
+ 
 }
