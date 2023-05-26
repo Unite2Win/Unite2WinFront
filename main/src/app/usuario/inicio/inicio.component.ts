1b38d37d669ffe7bd753201ede6e27bea8ddf88e
DESKTOP-TI6PHEJ\Eneko <dam32.eneko.sanchez@gmail.com>
2023-05-26 15:35:32 +0200

    añadido routing en inicio

--- main/src/app/usuario/inicio/inicio.component.ts
+++ main/src/app/usuario/inicio/inicio.component.ts
@@ -3,6 +3,7 @@ import { Objetivo } from '../interfaces/objetivo';
 import { ObjetivosService } from '../services/objetivos.service';
 import { globales } from 'common/globales';
 import { Usuario } from '../interfaces/usuarioModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -12,19 +13,25 @@ import { Usuario } from '../interfaces/usuarioModel';
 export class InicioComponent implements OnInit {
   objetivo: Objetivo;
   objetivos: Objetivo[] = [];
-  ObjetivosTitulo:String = 'Objetivos Activos:';
-  TextoBoton:String = 'Ver todos los objetivos';
+  ObjetivosTitulo: String = 'Objetivos Activos:';
+  TextoBoton: String = 'Ver todos los objetivos';
   globales: globales;
-  usuario:Usuario = globales.usuarioLogueado;
-  constructor(private objetivosService: ObjetivosService) { }
+  usuario: Usuario = globales.usuarioLogueado;
+
+  constructor(private objetivosService: ObjetivosService, private router: Router) { }
+
   async ngOnInit() {
     await this.objetivosService.getObjetivos();
-  this.objetivos = this.objetivosService.objetivosBd;  
-  console.log(this.objetivos.length);
-  if (this.objetivos.length == 0) {
-    this.ObjetivosTitulo = 'Aun no hay objetivos';
-    this.TextoBoton = 'Crear objetivos';
-  };
-}
- 
+    this.objetivos = this.objetivosService.objetivosBd;
+    console.log(this.objetivos.length);
+    if (this.objetivos.length == 0) {
+      this.ObjetivosTitulo = 'Aun no hay objetivos';
+      this.TextoBoton = 'Crear objetivos';
+    };
+  }
+
+  irObjetivos() {
+    this.router.navigate(['/usuario/objetivos']);
+  }
+
 }
