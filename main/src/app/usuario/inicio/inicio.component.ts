b76a6fb8455e85036bc4f36c673b69a7990101d2
DESKTOP-TI6PHEJ\Eneko <dam32.eneko.sanchez@gmail.com>
2023-05-27 13:44:06 +0200

    cambios de estilo en inicio

--- main/src/app/usuario/inicio/inicio.component.ts
+++ main/src/app/usuario/inicio/inicio.component.ts
@@ -23,15 +23,32 @@ export class InicioComponent implements OnInit {
   async ngOnInit() {
     await this.objetivosService.getObjetivos();
     this.objetivos = this.objetivosService.objetivosBd;
-    console.log(this.objetivos.length);
+    console.log(this.objetivos);
     if (this.objetivos.length == 0) {
       this.ObjetivosTitulo = 'Aun no hay objetivos';
       this.TextoBoton = 'Crear objetivos';
+    } else if (this.objetivos.length > 3) {
+      this.objetivos = this.objetivos.slice(0, 3);
     };
   }
 
   irObjetivos() {
     this.router.navigate(['/usuario/objetivos']);
+  }
+
+  getProgressBarValue(objetivo: Objetivo): number {
+    const fechaString = objetivo.create_date;
+    const fecha = new Date(fechaString);
+    //console.log(fecha);
+    const diferenciaEnDias = Math.floor((new Date().getTime() - fecha.getTime()) / (1000 * 60 * 60 * 24));
+    //console.log(diferenciaEnDias);
+    let progreso = (diferenciaEnDias / objetivo.duracion) * 100;
+    progreso = Math.min(progreso, 100); // Para que no supere el 100%
+    if (progreso === 100) { //Aqui se comprueba si esta ya al 100% el objetivo para modificarlo en la bd
+      objetivo.complete_date = new Date();
+    }
+    return progreso;
+    
   }
 
 }
